44715db6cb72726fbe0256eb16855cbca0242b7d
Tristan J. McNab <tristan.j.mcnab@gmail.com>
2022-04-09 15:34:28 -0400

    refactor a lot of useless shit out of the structure

--- website/components/Article.tsx
+++ website/components/Article.tsx
@@ -1,43 +1,16 @@
 import { ReactNode } from "react"
-import Flex from "./Flex"
-import Head from "next/head"
-import Link from "next/link"
-import Container from "./Container"
-import { useRouter } from "next/router"
-import Tag from "./Tag"
+import Layout from './Layout'
 
 interface Metadata {
     date?: Date,
-    tags: string['meta'],
+    tags: string[],
     title: string,
 }
 
-export default function Layout({ children, meta }: { children: ReactNode, metadata: Metadata }) {
-    const router = useRouter()
-    
-	return (
-		<>
-			<Head>
-				<title>{`${meta.title} - neuromancer.io`}</title>
-			</Head>
-			<Flex justifyContent="center">
-				<Container>
-                    <header>
-                        <h1>
-                            <a>netdir://</a>
-							<Link href='/'>tmcnab.pub</Link>
-                            <a>/</a>
-                            <Link href='/articles'>articles</Link>
-                            <a>/</a>
-                            <Link href={router.pathname}>{meta.title}</Link>
-						</h1>
-                        {meta.tags.map(tag => <Tag key={tag} label={tag} />)}
-                    </header>
-					<Flex as='main' flexDirection='column' justifyContent='center'>
-						{children}
-					</Flex>
-				</Container>
-			</Flex>
-		</>
+export default function Article ({ children, meta }: { children: ReactNode, metadata: Metadata }) {
+    return (
+		<article>
+			{children}
+		</article>
 	)
 }
--- website/components/Layout.tsx
+++ website/components/Layout.tsx
@@ -1,33 +1,30 @@
+import { Center, Column } from "./Flex"
 import { ReactNode } from "react"
-import Flex from "./Flex"
+import { useRouter } from "next/router"
 import Head from "next/head"
 import Link from "next/link"
-import Container from "./Container"
 
 export default function Layout({ children, title }: { children: ReactNode, title: string }) {
+	const router = useRouter()
+	console.info(router)
+
 	return (
 		<>
 			<Head>
 				<title>{`${title} - neuromancer.io`}</title>
 			</Head>
-			<Flex justifyContent="center">
-				<Container>
-					<Flex alignItems='center' as='header' flexDirection='row' justifyContent='space-between'>
-						<h1>
-							<Link href='/'>web.log</Link>
-						</h1>
-						<Flex alignItems='center' justifyContent='center'>
-							<form action='/' method='get' >
-									<input placeholder='Search' type='search' />
-									<button type='submit'>GO</button>
-							</form>
-						</Flex>
-					</Flex>
-					<Flex as='main' flexDirection='column' justifyContent='center'>
+			<Center className='container'>
+				<Column>
+					<header>
+						<nav>
+							<Link href='/'>netdir://tmcnab.github.io</Link>
+						</nav>
+					</header>
+					<main>
 						{children}
-					</Flex>
-				</Container>
-			</Flex>
+					</main>
+				</Column>
+			</Center>
 		</>
 	)
 }
